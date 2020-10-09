76132b9c6d554f57d6dff76bedd426e133ca1969
jordimj <jurado.jordi@gmail.com>
2020-10-09 12:32:29 +0200

    store modification to add success, pending and fail cases to api call

--- src/store/actions/actions.ts
+++ src/store/actions/actions.ts
@@ -1,20 +1,75 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
-import { FETCH_COUNTRY_STATS } from './types';
+import {
+  FETCH_COUNTRY_STATS_START,
+  FETCH_COUNTRY_STATS_SUCCESS,
+  FETCH_COUNTRY_STATS_FAIL,
+} from './types';
+import { CountryStatsData } from '../../shared/CountryStatsData';
 
-interface CountryStats {
-  type: string;
-  payload: {};
+interface FetchCountryStatsStartAction {
+  type: typeof FETCH_COUNTRY_STATS_START;
 }
+
+interface FetchCountryStatsSuccessAction {
+  type: typeof FETCH_COUNTRY_STATS_SUCCESS;
+  payload: {
+    timelineitems: { [key: string]: CountryStatsData }[];
+  };
+}
+
+interface FetchCountryStatsFailAction {
+  type: typeof FETCH_COUNTRY_STATS_FAIL;
+  error: Error | string;
+}
+
+export type FetchCountryStatsAction =
+  | FetchCountryStatsStartAction
+  | FetchCountryStatsSuccessAction
+  | FetchCountryStatsFailAction;
+
+const fetchCountryStatsStart = (): FetchCountryStatsStartAction => {
+  return {
+    type: FETCH_COUNTRY_STATS_START,
+  };
+};
+
+const fetchCountryStatsSuccess = (data: {
+  timelineitems: { [key: string]: CountryStatsData }[];
+}): FetchCountryStatsSuccessAction => {
+  return {
+    type: FETCH_COUNTRY_STATS_SUCCESS,
+    payload: data,
+  };
+};
+
+const fetchCountryStatsFail = (
+  error: Error | string
+): FetchCountryStatsFailAction => {
+  return {
+    type: FETCH_COUNTRY_STATS_FAIL,
+    error,
+  };
+};
 
 export const fetchCountryStats = (countryName: string) => {
   return async (dispatch: Dispatch) => {
-    const response = await axios.get(
-      `https://api.thevirustracker.com/free-api?countryTimeline=${countryName}`
-    );
-    dispatch<CountryStats>({
-      type: FETCH_COUNTRY_STATS,
-      payload: response.data,
-    });
+    dispatch<FetchCountryStatsStartAction>(fetchCountryStatsStart());
+
+    try {
+      const response = await axios.get(
+        `https://api.thevirustracker.com/free-api?countryTimeline=${countryName}`
+      );
+
+      if (response.data.timelineitems) {
+        dispatch<FetchCountryStatsSuccessAction>(
+          fetchCountryStatsSuccess(response.data)
+        );
+      } else {
+        dispatch<FetchCountryStatsFailAction>(fetchCountryStatsFail('No data'));
+      }
+    } catch (err) {
+      dispatch<FetchCountryStatsFailAction>(fetchCountryStatsFail(err));
+    }
   };
 };
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,24 +1,45 @@
-import { FETCH_COUNTRY_STATS } from './actions/types';
+import * as actionTypes from './actions/types';
+import { FetchCountryStatsAction } from './actions/actions';
 import { CountryStatsData } from '../shared/CountryStatsData';
 
+export interface RootState {
+  countryStats?: {
+    [key: string]: CountryStatsData;
+  };
+  loading: boolean;
+  error?: Error | string;
+}
+
 export const reducer = (
-  state = {},
-  action: {
-    type: string;
-    payload: {
-      timelineitems: { [key: string]: CountryStatsData | string }[];
-    };
-  }
-) => {
+  state = { loading: false },
+  action: FetchCountryStatsAction
+): RootState => {
   switch (action.type) {
-    case FETCH_COUNTRY_STATS:
+    case actionTypes.FETCH_COUNTRY_STATS_START:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case actionTypes.FETCH_COUNTRY_STATS_SUCCESS:
       if (action.payload.timelineitems[0].stat) {
         delete action.payload.timelineitems[0].stat;
       }
       return {
         ...state,
         countryStats: action.payload.timelineitems[0],
+        loading: false,
+        error: undefined,
       };
+
+    case actionTypes.FETCH_COUNTRY_STATS_FAIL:
+      return {
+        ...state,
+        countryStats: undefined,
+        error: action.error,
+        loading: false,
+      };
+
     default:
       return state;
   }
